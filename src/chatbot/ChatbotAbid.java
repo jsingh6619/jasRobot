c8df810ac15f54b84184bbf0fd19344b04d3cc72
ahossain3678 <ahossain3678@bths.edu>
2017-09-30 21:51:06 -0400

    ChatbotAbid

---
+++ src/chatbot/ChatbotAbid.java
@@ -0,0 +1,48 @@
+ package chatbot;
+//harsh chatbot
+public class ChatbotAbid implements Topic {
+	
+	private String[] keywords;
+	private String goodbyeKeyword;
+	private String secretKeyword;
+	private String response;
+	
+	public ChatbotAbid() {
+		String [] temp = {"stuff", "things", "whatever", "nothing"};
+		keywords = temp;
+		goodbyeKeyword = "bye";
+		secretKeyword = "mad";
+		response = "";
+	}
+	
+	@Override
+	public void talk(String response) {
+		ChatbotMain.print("");		
+		response = ChatbotMain.getInput();
+		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
+			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
+				ChatbotMain.print("I'M NOT MAD. STOP TALKING BEFORE I FAIL YOU.");
+				response = ChatbotMain.getInput();
+			}
+			else {
+				ChatbotMain.print("YOU'RE BORING.");
+				response = ChatbotMain.getInput();
+			}
+		}
+		//access variables from other classes
+		ChatbotMain.print("GET OUT OF MY FACE" + ChatbotMain.chatbot.getUsername() + "!");
+		ChatbotMain.chatbot.startChatting();
+	}
+	
+	@Override
+	public boolean isTriggered(String response) {
+		for (int i = 0; i < keywords.length; i++) {
+			//IMPORTANT (on the rubric)
+			if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
