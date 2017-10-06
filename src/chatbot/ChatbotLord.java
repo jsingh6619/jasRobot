308cbde6e6e4ed3dd6f47f23c3b41cd62ff44adb
lordr <lordr@DESKTOP-V7E85NB>
2017-10-06 15:18:28 -0400

    my stuff

--- src/chatbot/ChatbotLord.java
+++ src/chatbot/ChatbotLord.java
@@ -6,7 +6,6 @@ public class ChatbotLord implements Topic {
 	private String goodbyeKeyword;
 	private String secretKeyword;
 	private String response;
-	private boolean isPassed;
 	
 	public ChatbotLord() {
 
@@ -18,54 +17,30 @@ public class ChatbotLord implements Topic {
 	}
 	
 	public void talk(String response) {
-		ChatbotMain.print("SOOOOO you have questions or concerns about assessments. Why? Do you think you can pass???");
-			response = ChatbotMain.getInput();
+		
+		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
+			if(ChatbotMain.findKeyword(response, "test", 0) >= 0) 
+			{
+				ChatbotMain.print("SOOOOO you have questions or concerns about assessments. Why? Do you think you can pass???");
+				response = ChatbotMain.getInput();
 			
-			//doesn't work yet
-			
-			isPassed = false;
-			int answerpsn = ChatbotMain.findKeyword(response, "yes", 0);
-			
-			if(answerpsn >= 0) {
-				isPassed = true;
-			}
-			
-			if(isPassed) {
-				ChatbotMain.print("Ha jokes you imbecile");
-			}
-			else {
+				int answerpsn = ChatbotMain.findKeyword(response, "yes", 0);
 				
+				if(answerpsn >= 0) {
+					ChatbotMain.print("Ha jokes you imbecile");
+					ChatbotMain.chatbot.restartChat(ChatbotMain.getInput());
+				}
 			}
-			
-			
-			while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
-				if(ChatbotMain.chatbot.getAbed().isTriggered(response)) {
-					ChatbotMain.chatbot.getAbed().talk(response);
-					break;
-				}
-				if(ChatbotMain.chatbot.getJasmit().isTriggered(response)) {
-					ChatbotMain.chatbot.getJasmit().talk(response);
-					break;
-				}
-				if(ChatbotMain.chatbot.getAbid().isTriggered(response)) {
-					ChatbotMain.chatbot.getAbid().talk(response);
-					break;
-				}
-				
-				else {
+			else 
+			{
 					ChatbotMain.print("Yeah. That's pretty cool. But there are things I like even more tell me something else");
 					response = ChatbotMain.getInput();
-				}
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/jsingh6619/jasRobot.git
 			}
-			
-			//way to access variables from other classes
+		}
 			ChatbotMain.print("Well, it was nice talking to you, " + ChatbotMain.chatbot.getUsername() + "!");
 			ChatbotMain.chatbot.startChatting();
-		}
+		
+	}
 	
 	public boolean isTriggered(String response) {
 		for(int i = 0; i < keywords.length; i++)
