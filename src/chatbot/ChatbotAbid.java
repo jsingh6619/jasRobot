27e44ab6da74bc50499eb2b66b747b18d6ed9859
nasir <nasir@nas1>
2017-10-10 20:43:38 -0400

    fixes

--- src/chatbot/ChatbotAbid.java
+++ src/chatbot/ChatbotAbid.java
@@ -1,52 +1,79 @@
  package chatbot;
 //harsh chatbot
-public class ChatbotAbid implements Topic {
+public class ChatbotAbid implements Topic{
 	
 	private String[] keywords;
 	private String goodbyeKeyword;
-	private String secretKeyword;
 	private String response;
 	
+	
 	public ChatbotAbid() {
-		String [] temp = {"late","classwork","food","like"};
+		String [] temp = {"late","classwork","food",""};
 		
 		keywords = temp;
 		goodbyeKeyword = "bye";
-		secretKeyword = "grade";
 		response = "";
 	}
 	public void talk(String response) {
 		ChatbotMain.print("You're really starting to piss me off");
-		response = ChatbotMain.getInput();
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
 			//talking to chatbot
 			
+			ChatbotMain.print("I'm going to kill you. Here's a gun we're dueling");
 			
+			ChatbotMain.print("Do you know how to play?");
+			response = ChatbotMain.getInput();
+			if(ChatbotMain.findKeyword(response, "yes", 0) >= 0){
+				ChatbotMain.print("Okay good. Type \"0\" to load \"1\" to shoot an \"2\" to guard");
+			}
+			else {
+				ChatbotMain.print("I work at Tech so I don't explain anything");
+				ChatbotMain.print("But I will tell you, type \"0\" to load \"1\" to shoot an \"2\" to guard ");
+			}
+			ChatbotMain.print("You ready?");
+			response = ChatbotMain.getInput();
+			ChatbotMain.print("That's what I like to hear");
 			
-			//interconnectivity
+			Duel duel = new Duel();
+			boolean isMyAlgorithmAmazing = duel.dukeItOut();
+			//play duel
+			
+			if(isMyAlgorithmAmazing) {
+				ChatbotMain.print("HAH TAKE THAT L");
+			}
+			else {
+				ChatbotMain.print("I can see why everybody talks about you behind your back.");
+			}
+			
 			ChatbotMain.print("Do you like this class?");
 			response = ChatbotMain.getInput();
 				if(ChatbotMain.findKeyword(response, "yes", 0) >= 0){
-					ChatbotMain.print("Thanks. You better like this class");
+					ChatbotMain.print("You better like this class");
 					ChatbotMain.print("What else do you like?");
 					ChatbotMain.getInput();
 				}
 				else {
+					ChatbotMain.print("*puts 0 into your pupilpath*");
 					ChatbotMain.print("Then what do you like?");
 					ChatbotMain.getInput();
+					
 				}
-				if(ChatbotMain.findKeyword(response, "like", 0) >= 0) {
-					int likePsn = response.indexOf("like");
-					String like = response.substring(likePsn+5);
-					ChatbotMain.print("Oh you like " + like + ", that explains why you're single");
-					response = ChatbotMain.getInput();
+	
+					if(ChatbotMain.findKeyword(response, "like", 0) >= 0) {
+						int likePsn = response.indexOf("like");
+						String like = response.substring(likePsn+5);
+						ChatbotMain.print("Oh you like " + like + ", that explains why you're single");
+						response = ChatbotMain.getInput();
+					}
+					else {
+						ChatbotMain.print("Alright be like that.");
+					}
+			
 				}
 			
-			
-		ChatbotMain.print("Talk about something else" + ChatbotMain.chatbot.getUsername() + "!");
-		ChatbotMain.chatbot.startChatting();
+			ChatbotMain.print("You're putting me to sleep and I'm not even real. Talk about something else " + ChatbotMain.chatbot.getUsername() + " !");
+		
 		}
-	}
 	public boolean isTriggered(String response) {
 		for (int i = 0; i < keywords.length; i++) {
 			//IMPORTANT (on the rubric)
@@ -58,3 +85,4 @@ public class ChatbotAbid implements Topic {
 	}
 
 }
+
--- src/chatbot/ChatbotLord.java
+++ src/chatbot/ChatbotLord.java
@@ -4,43 +4,156 @@ public class ChatbotLord implements Topic {
 
 	private String[] keywords;
 	private String goodbyeKeyword;
-	private String secretKeyword;
 	private String response;
+	private String study;
 	
 	public ChatbotLord() {
 
-		String[] temp = {"test","quiz","tests","quizzes"};
+		String[] temp = {"test","quiz","tests","quizzes","mean","score","scoring","scored"};
 		keywords = temp;
 		goodbyeKeyword = "bye";
-		secretKeyword = "long";
 		response = "";
+		study = "";
 	}
 	
 	public void talk(String response) {
 		
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
-			if(ChatbotMain.findKeyword(response, "test", 0) >= 0) 
+			if(ChatbotMain.findKeyword(response, "test", 0) >= 0 || ChatbotMain.findKeyword(response, "tests", 0) >= 0 
+					|| ChatbotMain.findKeyword(response, "quiz", 0) >= 0 || ChatbotMain.findKeyword(response, "quizzes", 0) >= 0) 
 			{
-				ChatbotMain.print("SOOOOO you have questions or concerns about assessments. Why? Do you think you can pass???");
+				
+				ChatbotMain.print("SOOO you have concerns about assessments. Why? Do you think you can pass???");
 				response = ChatbotMain.getInput();
 			
-				int answerpsn = ChatbotMain.findKeyword(response, "yes", 0);
-				
-				if(answerpsn >= 0) {
-					ChatbotMain.print("Ha jokes you imbecile");
+				if(ChatbotMain.findKeyword(response, "yes", 0) >= 0 || ChatbotMain.findKeyword(response, "yea", 0) >= 0 || ChatbotMain.findKeyword(response, "sure", 0) >= 0) 
+				{
+					ChatbotMain.print("Ha you're funny I doubt it, but do you have any questions on test scoring in the hope of you ever passing? ");
+					
+					response = ChatbotMain.getInput();
+					
+					
+					if(ChatbotMain.findKeyword(response, "yes", 0) >= 0 || ChatbotMain.findKeyword(response, "yea", 0) >= 0) 
+					{
+						
+						ChatbotMain.print("Test usually have about 15 to 20 questions. 5 Short Answer and the rest Multiple Choice,"
+								+ " sometimes I may make a game for you to complete in a limited amount of time. How about we play a little game?");
+						response = ChatbotMain.getInput();
+						
+						if(ChatbotMain.findKeyword(response, "yes", 0) >= 0 || ChatbotMain.findKeyword(response, "yea", 0) >= 0 
+								|| ChatbotMain.findKeyword(response, "sure", 0) >= 0 || ChatbotMain.findKeyword(response, "ok", 0) >= 0) 
+						{
+							guessingGame();
+						}
+						
+						ChatbotMain.chatbot.restartChat(ChatbotMain.getInput());
+					}
+				}
+				else {
+					ChatbotMain.print("How do you study for exams?");
+					study = ChatbotMain.getInput();
+					
+					studying(study);
 					ChatbotMain.chatbot.restartChat(ChatbotMain.getInput());
 				}
 			}
-			else 
+			
+			else if(ChatbotMain.findKeyword(response, "mean", 0) >= 0) 
 			{
+				ChatbotMain.print("Why do you think I'm so mean, you're not ready for this class");
+				ChatbotMain.chatbot.restartChat(ChatbotMain.getInput());
+			}
+			
+			else if((ChatbotMain.findKeyword(response, "score", 0) >= 0 || ChatbotMain.findKeyword(response, "scored", 0) >= 0) && (ChatbotMain.findKeyword(response, "how", 0) >= 0))
+			{
+				
+				ChatbotMain.print("Test usually have about 15 to 20 questions. 5 Short Answer and the rest Multiple Choice, sometimes I may make a game for you to complete in a limited amount of time. How about we play a little game?");
+				response = ChatbotMain.getInput();
+				
+				if(ChatbotMain.findKeyword(response, "yes", 0) >= 0 || ChatbotMain.findKeyword(response, "yea", 0) >= 0) 
+				{
+					guessingGame();
+				}
+				
+				ChatbotMain.chatbot.restartChat(ChatbotMain.getInput());
+			}
+			
+			else {
 					ChatbotMain.print("Yeah. That's pretty cool. But there are things I like even more tell me something else");
 					response = ChatbotMain.getInput();
+					ChatbotMain.chatbot.restartChat(ChatbotMain.getInput());
 			}
 		}
 			ChatbotMain.print("Well, it was nice talking to you, " + ChatbotMain.chatbot.getUsername() + "!");
 			ChatbotMain.chatbot.startChatting();
 		
+		}
+
+	
+	public void guessingGame()
+	{
+		int chatGuess = (int) (Math.random() * 100);
+		ChatbotMain.print("So based on our interaction, I've come up with a guess of what you'll get on my first test. Guess it!");
+		
+		response = ChatbotMain.getInput();
+		
+		
+		while(Integer.parseInt(response) != chatGuess) 
+		{
+			if(Integer.parseInt(response) > chatGuess) {
+				ChatbotMain.print("Too high, you really thought you were smart huh. Guess Again!");
+				response = ChatbotMain.getInput();
+			}
+			else {
+				ChatbotMain.print("Too low, Hey I didn't think you were that incompetent. Guess Again!");
+				response = ChatbotMain.getInput();
+			}
+		}
+		
+		if(chatGuess < 65) {
+		ChatbotMain.print("You are correct, I think you'll get a " + chatGuess + " on the first test!, Yes you're going to fail");
+		
+		ChatbotMain.print("How do you even study for exams?");
+		study = ChatbotMain.getInput();
+		
+		studying(study);
+		}
+		
+		else {
+			ChatbotMain.print("You are correct, I think you'll get a " + chatGuess + " on the first test!, Wow you're going to pass be happy \\ What other boring stuff do you want to talk about" );
+		}
 	}
+	
+	
+	public void studying(String response) {
+		
+		if(ChatbotMain.findKeyword(response, "study", 0) >= 0) {
+			
+			int studyPsn = response.indexOf("study");
+			String study = response.substring(studyPsn + 6);
+			ChatbotMain.print("The reason why you're going to fail is because you are currently " + study + ", and there are smarter strategies to increase your grade \\What other boring stuff do you want to talk about");
+		}
+		else if(ChatbotMain.findKeyword(response, "study by", 0) >= 0) {
+			int studyPsn = response.indexOf("study by");
+			String study = response.substring(studyPsn + 9);
+			ChatbotMain.print("The reason why you're going to fail is because you are currently " + study + ", and there are smarter strategies to increase your grade \\What other boring stuff do you want to talk about");
+		}
+		else if(ChatbotMain.findKeyword(response, "i", 0) >= 0) {
+			int studyPsn = response.indexOf("i");
+			String study = response.substring(studyPsn + 2);
+			ChatbotMain.print("The reason why you're going to fail is because you " + study + ", and there are smarter strategies to increase your grade \\What other boring stuff do you want to talk about");
+		}
+		else if(ChatbotMain.findKeyword(response, "with", 0) >= 0) {
+			int studyPsn = response.indexOf("with");
+			String study = response.substring(studyPsn + 2);
+			ChatbotMain.print("The reason why you're going to fail is because you use " + study + ", and there are smarter strategies to increase your grade \\What other boring stuff do you want to talk about");
+		}
+		else {
+			String study = response;
+			ChatbotMain.print("The reason why you're going to fail is because you " + study + ", and there are smarter strategies to increase your grade \\What other boring stuff do you want to talk about");
+		}
+	}
+	
 	
 	public boolean isTriggered(String response) {
 		for(int i = 0; i < keywords.length; i++)
